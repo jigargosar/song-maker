2e573a63d5ac2176fd1cc317aa81cb87d902ca0c
Jigar Gosar <jigar.gosar@gmail.com>
2025-09-19 13:17:36 +0530

    Reorder Patterns.elm: common functions on top, ordered patterns, ordered configs for clarity and maintainability

--- src/Patterns.elm
+++ src/Patterns.elm
@@ -1,13 +1,18 @@
 module Patterns exposing
     ( PatternConfig
     , twinkleTwinkleChordsConfig
+    , twinkleTwinkleChordsPattern
     , twinkleTwinkleChordsV2Config
     , twinkleTwinkleChordsV3Config
     , twinkleTwinkleConfig
+    , twinkleTwinklePattern
+    , twinkleTwinkleV2Pattern
+    , twinkleTwinkleV3Pattern
     , vShapeConfig
+    , vShapePattern
     )
 
--- PATTERN CONFIGURATION TYPE
+-- COMMON TYPES AND FUNCTIONS
 
 
 type alias PatternConfig =
@@ -20,10 +25,6 @@ type alias PatternConfig =
     , octaveStart : Int
     , octaveCount : Int
     }
-
-
-
--- GRID GENERATION FUNCTIONS
 
 
 toggleGridCell : Int -> Int -> List (List Bool) -> List (List Bool)
@@ -163,8 +164,7 @@ notesToGrid o =
 
 
 
--- PATTERN DATA
--- V-shaped melody demo: smooth stepwise progression C3 -> C4 -> C3
+-- ORDERED LIST OF PATTERNS
 
 
 vShapePattern : List (List String)
@@ -176,7 +176,7 @@ vShapePattern =
     , [ "G3" ]
     , [ "A3" ]
     , [ "B3" ]
-    , [ "C4" ] -- Ascending
+    , [ "C4" ]
     , [ "C4" ]
     , [ "B3" ]
     , [ "A3" ]
@@ -184,12 +184,8 @@ vShapePattern =
     , [ "F3" ]
     , [ "E3" ]
     , [ "D3" ]
-    , [ "C3" ] -- Descending
+    , [ "C3" ]
     ]
-
-
-
--- Twinkle Twinkle Little Star full melody pattern (64 steps)
 
 
 twinkleTwinklePattern : List (List String)
@@ -197,314 +193,271 @@ twinkleTwinklePattern =
     [ [ "C4" ]
     , [ "C4" ]
     , [ "G4" ]
-    , [ "G4" ] -- Twinkle twinkle
+    , [ "G4" ]
     , [ "A4" ]
     , [ "A4" ]
     , [ "G4" ]
-    , [] -- little star
+    , []
     , [ "F4" ]
     , [ "F4" ]
     , [ "E4" ]
-    , [ "E4" ] -- How I wonder
+    , [ "E4" ]
     , [ "D4" ]
     , [ "D4" ]
     , [ "C4" ]
-    , [] -- what you are
+    , []
     , [ "G4" ]
     , [ "G4" ]
     , [ "F4" ]
-    , [ "F4" ] -- Up above the
+    , [ "F4" ]
     , [ "E4" ]
     , [ "E4" ]
     , [ "D4" ]
-    , [] -- world so high
+    , []
     , [ "G4" ]
     , [ "G4" ]
     , [ "F4" ]
-    , [ "F4" ] -- Like a diamond
+    , [ "F4" ]
     , [ "E4" ]
     , [ "E4" ]
     , [ "D4" ]
-    , [] -- in the sky
+    , []
     , [ "C4" ]
     , [ "C4" ]
     , [ "G4" ]
-    , [ "G4" ] -- Twinkle twinkle (repeat)
+    , [ "G4" ]
     , [ "A4" ]
     , [ "A4" ]
     , [ "G4" ]
-    , [] -- little star
+    , []
     , [ "F4" ]
     , [ "F4" ]
     , [ "E4" ]
-    , [ "E4" ] -- How I wonder
+    , [ "E4" ]
     , [ "D4" ]
     , [ "D4" ]
     , [ "C4" ]
-    , [] -- what you are
+    , []
     , [ "G4" ]
     , [ "G4" ]
     , [ "F4" ]
-    , [ "F4" ] -- Up above the
+    , [ "F4" ]
     , [ "E4" ]
     , [ "E4" ]
     , [ "D4" ]
-    , [] -- world so high
+    , []
     , [ "G4" ]
     , [ "G4" ]
     , [ "F4" ]
-    , [ "F4" ] -- Like a diamond
+    , [ "F4" ]
     , [ "E4" ]
     , [ "E4" ]
     , [ "D4" ]
-    , [] -- in the sky
+    , []
     ]
 
 
-
--- Twinkle Twinkle Little Star with chords (chords only on key steps, 64 steps)
--- Twinkle Twinkle V2: cleaner chord arrangement with brighter harmony
--- Twinkle Twinkle V3: chords always above C4, no low-pitch chords
-
-
-twinkleTwinkleV3Pattern : List (List String)
-twinkleTwinkleV3Pattern =
-    [ [ "C4", "E4", "G4", "C5" ] -- Twin-kle
-    , [ "C4" ] -- twin-kle
-    , [ "G4", "B4", "D5", "G5" ] -- lit-tle
-    , [ "G4" ] -- star
-    , [ "A4", "C5", "E5", "A5" ] -- How
-    , [ "A4" ] -- I
-    , [ "G4", "B4", "D5", "G5" ] -- won-der
-    , [] -- what
-    , [ "F4", "A4", "C5", "F5" ] -- you
-    , [ "F4" ] -- are
-    , [ "E4", "G4", "C5", "E5" ] --
-    , [ "E4" ] --
-    , [ "D4", "G4", "B4", "D5" ] --
-    , [ "D4" ] --
-    , [ "C4", "E4", "G4", "C5" ] --
-    , [] --
-    , [ "G4", "B4", "D5", "G5" ] -- Up
-    , [ "G4" ] -- a-
-    , [ "F4", "A4", "C5", "F5" ] -- bove
-    , [ "F4" ] -- the
-    , [ "E4", "G4", "C5", "E5" ] -- world
-    , [ "E4" ] -- so
-    , [ "D4", "G4", "B4", "D5" ] -- high
-    , [] --
-    , [ "G4", "B4", "D5", "G5" ] -- Like
-    , [ "G4" ] -- a
-    , [ "F4", "A4", "C5", "F5" ] -- dia-
-    , [ "F4" ] -- mond
-    , [ "E4", "G4", "C5", "E5" ] -- in
-    , [ "E4" ] -- the
-    , [ "D4", "G4", "B4", "D5" ] -- sky
-    , [] --
-    , [ "C4", "E4", "G4", "C5" ] -- Twin-kle
-    , [ "C4" ] -- twin-kle
-    , [ "G4", "B4", "D5", "G5" ] -- lit-tle
-    , [ "G4" ] -- star
-    , [ "A4", "C5", "E5", "A5" ] -- How
-    , [ "A4" ] -- I
-    , [ "G4", "B4", "D5", "G5" ] -- won-der
-    , [] -- what
-    , [ "F4", "A4", "C5", "F5" ] -- you
-    , [ "F4" ] -- are
-    , [ "E4", "G4", "C5", "E5" ] --
-    , [ "E4" ] --
-    , [ "D4", "G4", "B4", "D5" ] --
-    , [ "D4" ] --
-    , [ "C4", "E4", "G4", "C5" ] --
-    , [] --
+twinkleTwinkleChordsPattern : List (List String)
+twinkleTwinkleChordsPattern =
+    [ [ "C4", "E4", "G4" ]
+    , [ "C4" ]
+    , [ "G4" ]
+    , [ "G4" ]
+    , [ "A4", "C5", "E5" ]
+    , [ "A4" ]
+    , [ "G4" ]
+    , []
+    , [ "F4", "A4", "C5" ]
+    , [ "F4" ]
+    , [ "E4" ]
+    , [ "E4" ]
+    , [ "D4", "F4", "A4" ]
+    , [ "D4" ]
+    , [ "C4" ]
+    , []
+    , [ "G4", "B4", "D5" ]
+    , [ "G4" ]
+    , [ "F4" ]
+    , [ "F4" ]
+    , [ "E4", "G4", "C5" ]
+    , [ "E4" ]
+    , [ "D4" ]
+    , []
+    , [ "G4", "B4", "D5" ]
+    , [ "G4" ]
+    , [ "F4" ]
+    , [ "F4" ]
+    , [ "E4", "G4", "C5" ]
+    , [ "E4" ]
+    , [ "D4" ]
+    , []
+    , [ "C4", "E4", "G4" ]
+    , [ "C4" ]
+    , [ "G4" ]
+    , [ "G4" ]
+    , [ "A4", "C5", "E5" ]
+    , [ "A4" ]
+    , [ "G4" ]
+    , []
+    , [ "F4", "A4", "C5" ]
+    , [ "F4" ]
+    , [ "E4" ]
+    , [ "E4" ]
+    , [ "D4", "F4", "A4" ]
+    , [ "D4" ]
+    , [ "C4" ]
+    , []
+    , [ "G4", "B4", "D5" ]
+    , [ "G4" ]
+    , [ "F4" ]
+    , [ "F4" ]
+    , [ "E4", "G4", "C5" ]
+    , [ "E4" ]
+    , [ "D4" ]
+    , []
+    , [ "G4", "B4", "D5" ]
+    , [ "G4" ]
+    , [ "F4" ]
+    , [ "F4" ]
+    , [ "E4", "G4", "C5" ]
+    , [ "E4" ]
+    , [ "D4" ]
+    , []
     ]
 
 
 twinkleTwinkleV2Pattern : List (List String)
 twinkleTwinkleV2Pattern =
-    [ [ "C4", "C5", "E5", "G5" ] -- Twin-kle
-    , [ "C4" ] -- twin-kle
-    , [ "G4", "G5", "B5", "D6" ] -- lit-tle
-    , [ "G4" ] -- star
-    , [ "A4", "A5", "C6", "E6" ] -- How
-    , [ "A4" ] -- I
-    , [ "G4", "G5", "B5", "D6" ] -- won-der
-    , [] -- what
-    , [ "F4", "F5", "A5", "C6" ] -- you
-    , [ "F4" ] -- are
-    , [ "E4", "C5", "E5", "G5" ] --
-    , [ "E4" ] --
-    , [ "D4", "G5", "B5", "D6" ] --
-    , [ "D4" ] --
-    , [ "C4", "C5", "E5", "G5" ] --
-    , [] --
-    , [ "G4", "G5", "B5", "D6" ] -- Up
-    , [ "G4" ] -- a-
-    , [ "F4", "F5", "A5", "C6" ] -- bove
-    , [ "F4" ] -- the
-    , [ "E4", "C5", "E5", "G5" ] -- world
-    , [ "E4" ] -- so
-    , [ "D4", "G5", "B5", "D6" ] -- high
-    , [] --
-    , [ "G4", "G5", "B5", "D6" ] -- Like
-    , [ "G4" ] -- a
-    , [ "F4", "F5", "A5", "C6" ] -- dia-
-    , [ "F4" ] -- mond
-    , [ "E4", "C5", "E5", "G5" ] -- in
-    , [ "E4" ] -- the
-    , [ "D4", "G5", "B5", "D6" ] -- sky
-    , [] --
-    , [ "C4", "C5", "E5", "G5" ] -- Twin-kle
-    , [ "C4" ] -- twin-kle
-    , [ "G4", "G5", "B5", "D6" ] -- lit-tle
-    , [ "G4" ] -- star
-    , [ "A4", "A5", "C6", "E6" ] -- How
-    , [ "A4" ] -- I
-    , [ "G4", "G5", "B5", "D6" ] -- won-der
-    , [] -- what
-    , [ "F4", "F5", "A5", "C6" ] -- you
-    , [ "F4" ] -- are
-    , [ "E4", "C5", "E5", "G5" ] --
-    , [ "E4" ] --
-    , [ "D4", "G5", "B5", "D6" ] --
-    , [ "D4" ] --
-    , [ "C4", "C5", "E5", "G5" ] --
-    , [] --
-    , [ "G4", "G5", "B5", "D6" ] -- When
-    , [ "G4" ] -- the
-    , [ "F4", "F5", "A5", "C6" ] -- bla-
-    , [ "F4" ] -- zing
-    , [ "E4", "C5", "E5", "G5" ] -- sun
-    , [ "E4" ] -- is
-    , [ "D4", "G5", "B5", "D6" ] -- gone
-    , [] --
-    , [ "G4", "G5", "B5", "D6" ] -- When
-    , [ "G4" ] -- he
-    , [ "F4", "F5", "A5", "C6" ] -- no-
-    , [ "F4" ] -- thing
-    , [ "E4", "C5", "E5", "G5" ] -- shines
-    , [ "E4" ] -- up-
-    , [ "D4", "G5", "B5", "D6" ] -- on
-    , [] --
-    , [ "C4", "C5", "E5", "G5" ] -- Then
-    , [ "C4" ] -- you
-    , [ "G4", "G5", "B5", "D6" ] -- show
-    , [ "G4" ] -- your
-    , [ "A4", "A5", "C6", "E6" ] -- lit-
-    , [ "A4" ] -- tle
-    , [ "G4", "G5", "B5", "D6" ] -- light
-    , [] --
-    , [ "F4", "F5", "A5", "C6" ] -- Twin-
-    , [ "F4" ] -- kle
-    , [ "E4", "C5", "E5", "G5" ] -- twin-
-    , [ "E4" ] -- kle
-    , [ "D4", "G5", "B5", "D6" ] -- all
-    , [ "D4" ] -- the
-    , [ "C4", "C5", "E5", "G5" ] -- night
-    , [] --
+    [ [ "C4", "C5", "E5", "G5" ]
+    , [ "C4" ]
+    , [ "G4", "G5", "B5", "D6" ]
+    , [ "G4" ]
+    , [ "A4", "A5", "C6", "E6" ]
+    , [ "A4" ]
+    , [ "G4", "G5", "B5", "D6" ]
+    , []
+    , [ "F4", "F5", "A5", "C6" ]
+    , [ "F4" ]
+    , [ "E4", "C5", "E5", "G5" ]
+    , [ "E4" ]
+    , [ "D4", "G5", "B5", "D6" ]
+    , [ "D4" ]
+    , [ "C4", "C5", "E5", "G5" ]
+    , []
+    , [ "G4", "G5", "B5", "D6" ]
+    , [ "G4" ]
+    , [ "F4", "F5", "A5", "C6" ]
+    , [ "F4" ]
+    , [ "E4", "C5", "E5", "G5" ]
+    , [ "E4" ]
+    , [ "D4", "G5", "B5", "D6" ]
+    , []
+    , [ "G4", "G5", "B5", "D6" ]
+    , [ "G4" ]
+    , [ "F4", "F5", "A5", "C6" ]
+    , [ "F4" ]
+    , [ "E4", "C5", "E5", "G5" ]
+    , [ "E4" ]
+    , [ "D4", "G5", "B5", "D6" ]
+    , []
+    , [ "C4", "C5", "E5", "G5" ]
+    , [ "C4" ]
+    , [ "G4", "G5", "B5", "D6" ]
+    , [ "G4" ]
+    , [ "A4", "A5", "C6", "E6" ]
+    , [ "A4" ]
+    , [ "G4", "G5", "B5", "D6" ]
+    , []
+    , [ "F4", "F5", "A5", "C6" ]
+    , [ "F4" ]
+    , [ "E4", "C5", "E5", "G5" ]
+    , [ "E4" ]
+    , [ "D4", "G5", "B5", "D6" ]
+    , [ "D4" ]
+    , [ "C4", "C5", "E5", "G5" ]
+    , []
+    , [ "G4", "G5", "B5", "D6" ]
+    , [ "G4" ]
+    , [ "F4", "F5", "A5", "C6" ]
+    , [ "F4" ]
+    , [ "E4", "C5", "E5", "G5" ]
+    , [ "E4" ]
+    , [ "D4", "G5", "B5", "D6" ]
+    , []
+    , [ "C4", "C5", "E5", "G5" ]
+    , [ "C4" ]
+    , [ "G4", "G5", "B5", "D6" ]
+    , [ "G4" ]
+    , [ "A4", "A5", "C6", "E6" ]
+    , [ "A4" ]
+    , [ "G4", "G5", "B5", "D6" ]
+    , []
+    , [ "F4", "F5", "A5", "C6" ]
+    , [ "F4" ]
+    , [ "E4", "C5", "E5", "G5" ]
+    , [ "E4" ]
+    , [ "D4", "G5", "B5", "D6" ]
+    , [ "D4" ]
+    , [ "C4", "C5", "E5", "G5" ]
+    , []
     ]
 
 
-twinkleTwinkleChordsPattern : List (List String)
-twinkleTwinkleChordsPattern =
-    [ [ "C4", "E4", "G4" ] -- chord (bar 1)
+twinkleTwinkleV3Pattern : List (List String)
+twinkleTwinkleV3Pattern =
+    [ [ "C4", "E4", "G4", "C5" ]
     , [ "C4" ]
+    , [ "G4", "B4", "D5", "G5" ]
     , [ "G4" ]
-    , [ "G4" ]
-    , [ "A4", "C5", "E5" ] -- chord (bar 2)
+    , [ "A4", "C5", "E5", "A5" ]
     , [ "A4" ]
-    , [ "G4" ]
+    , [ "G4", "B4", "D5", "G5" ]
     , []
-    , [ "F4", "A4", "C5" ] -- chord (bar 3)
+    , [ "F4", "A4", "C5", "F5" ]
     , [ "F4" ]
+    , [ "E4", "G4", "C5", "E5" ]
     , [ "E4" ]
-    , [ "E4" ]
-    , [ "D4", "F4", "A4" ] -- chord (bar 4)
+    , [ "D4", "G4", "B4", "D5" ]
     , [ "D4" ]
+    , [ "C4", "E4", "G4", "C5" ]
+    , []
+    , [ "G4", "B4", "D5", "G5" ]
+    , [ "G4" ]
+    , [ "F4", "A4", "C5", "F5" ]
+    , [ "F4" ]
+    , [ "E4", "G4", "C5", "E5" ]
+    , [ "E4" ]
+    , [ "D4", "G4", "B4", "D5" ]
+    , []
+    , [ "G4", "B4", "D5", "G5" ]
+    , [ "G4" ]
+    , [ "F4", "A4", "C5", "F5" ]
+    , [ "F4" ]
+    , [ "E4", "G4", "C5", "E5" ]
+    , [ "E4" ]
+    , [ "D4", "G4", "B4", "D5" ]
+    , []
+    , [ "C4", "E4", "G4", "C5" ]
     , [ "C4" ]
-    , []
-    , [ "G4", "B4", "D5" ] -- chord (bar 5)
+    , [ "G4", "B4", "D5", "G5" ]
     , [ "G4" ]
-    , [ "F4" ]
-    , [ "F4" ]
-    , [ "E4", "G4", "C5" ] -- chord (bar 6)
-    , [ "E4" ]
-    , [ "D4" ]
-    , []
-    , [ "G4", "B4", "D5" ] -- chord (bar 7)
-    , [ "G4" ]
-    , [ "F4" ]
-    , [ "F4" ]
-    , [ "E4", "G4", "C5" ] -- chord (bar 8)
-    , [ "E4" ]
-    , [ "D4" ]
-    , []
-    , [ "C4", "E4", "G4" ] -- chord (bar 9)
-    , [ "C4" ]
-    , [ "G4" ]
-    , [ "G4" ]
-    , [ "A4", "C5", "E5" ] -- chord (bar 10)
+    , [ "A4", "C5", "E5", "A5" ]
     , [ "A4" ]
-    , [ "G4" ]
+    , [ "G4", "B4", "D5", "G5" ]
     , []
-    , [ "F4", "A4", "C5" ] -- chord (bar 11)
+    , [ "F4", "A4", "C5", "F5" ]
     , [ "F4" ]
+    , [ "E4", "G4", "C5", "E5" ]
     , [ "E4" ]
-    , [ "E4" ]
-    , [ "D4", "F4", "A4" ] -- chord (bar 12)
+    , [ "D4", "G4", "B4", "D5" ]
     , [ "D4" ]
-    , [ "C4" ]
-    , []
-    , [ "G4", "B4", "D5" ] -- chord (bar 13)
-    , [ "G4" ]
-    , [ "F4" ]
-    , [ "F4" ]
-    , [ "E4", "G4", "C5" ] -- chord (bar 14)
-    , [ "E4" ]
-    , [ "D4" ]
-    , []
-    , [ "G4", "B4", "D5" ] -- chord (bar 15)
-    , [ "G4" ]
-    , [ "F4" ]
-    , [ "F4" ]
-    , [ "E4", "G4", "C5" ] -- chord (bar 16)
-    , [ "E4" ]
-    , [ "D4" ]
+    , [ "C4", "E4", "G4", "C5" ]
     , []
     ]
 
 
 
--- PATTERN CONFIGURATIONS
-
-
-twinkleTwinkleChordsV3Config : PatternConfig
-twinkleTwinkleChordsV3Config =
-    let
-        octaveStart =
-            4
-
-        octaveCount =
-            2
-
-        stepCount =
-            List.length twinkleTwinkleV3Pattern
-    in
-    { grid =
-        notesToGrid
-            { stepsWithNotes = twinkleTwinkleV3Pattern
-            , octaveStart = octaveStart
-            , octaveCount = octaveCount
-            , stepCount = stepCount
-            }
-    , bpm = 120
-    , barCount = 13
-    , beatsPerBar = 2
-    , splitBeats = 2
-    , name = "Twinkle Chords V3"
-    , octaveStart = octaveStart
-    , octaveCount = octaveCount
-    }
+-- ORDERED LIST OF CONFIGS
 
 
 vShapeConfig : PatternConfig
@@ -618,6 +571,35 @@ twinkleTwinkleChordsV2Config =
     , beatsPerBar = 2
     , splitBeats = 2
     , name = "Twinkle Chords V2"
+    , octaveStart = octaveStart
+    , octaveCount = octaveCount
+    }
+
+
+twinkleTwinkleChordsV3Config : PatternConfig
+twinkleTwinkleChordsV3Config =
+    let
+        octaveStart =
+            4
+
+        octaveCount =
+            2
+
+        stepCount =
+            List.length twinkleTwinkleV3Pattern
+    in
+    { grid =
+        notesToGrid
+            { stepsWithNotes = twinkleTwinkleV3Pattern
+            , octaveStart = octaveStart
+            , octaveCount = octaveCount
+            , stepCount = stepCount
+            }
+    , bpm = 120
+    , barCount = 13
+    , beatsPerBar = 2
+    , splitBeats = 2
+    , name = "Twinkle Chords V3"
     , octaveStart = octaveStart
     , octaveCount = octaveCount
     }
