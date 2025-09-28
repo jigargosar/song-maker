54e006f4d801e125dc7609501de1e7da02b27587
Jigar Gosar <jigar.gosar@gmail.com>
2025-09-28 14:13:14 +0530

    temp commit for grid, will hve to revert if we cant get much benifits.

---
+++ src/Grid.elm
@@ -0,0 +1,83 @@
+module Grid exposing
+    ( PitchPos
+    , PitchGrid
+    , PercPos
+    , PercGrid
+    , percPositionToTuple
+    , isPitchCellActive
+    , updatePitchCell
+    , isPercCellActive
+    , updatePercCell
+    , convertPercussionToGrid
+    )
+
+import Set exposing (Set)
+import Instruments exposing (PercType)
+
+
+-- Types
+
+type alias PitchPos =
+    { pitchIdx : Int, stepIdx : Int }
+
+type alias PitchGrid =
+    Set ( Int, Int )
+
+type alias PercPos =
+    { percType : PercType, stepIdx : Int }
+
+type alias PercGrid =
+    Set ( Int, Int )
+
+
+-- Conversion Functions
+
+percPositionToTuple : PercPos -> ( Int, Int )
+percPositionToTuple { percType, stepIdx } =
+    ( Instruments.percRowIdx percType, stepIdx )
+
+
+-- Cell State Management
+
+isPitchCellActive : PitchPos -> PitchGrid -> Bool
+isPitchCellActive { pitchIdx, stepIdx } pitchGrid =
+    Set.member ( pitchIdx, stepIdx ) pitchGrid
+
+updatePitchCell : PitchPos -> Bool -> PitchGrid -> PitchGrid
+updatePitchCell { pitchIdx, stepIdx } isActive pitchGrid =
+    if isActive then
+        Set.insert ( pitchIdx, stepIdx ) pitchGrid
+    else
+        Set.remove ( pitchIdx, stepIdx ) pitchGrid
+
+isPercCellActive : PercPos -> PercGrid -> Bool
+isPercCellActive position grid =
+    Set.member (percPositionToTuple position) grid
+
+updatePercCell : PercPos -> Bool -> PercGrid -> PercGrid
+updatePercCell position isActive grid =
+    let
+        tuple =
+            percPositionToTuple position
+    in
+    if isActive then
+        Set.insert tuple grid
+    else
+        Set.remove tuple grid
+
+
+-- Percussion Grid Conversion
+
+convertPercussionToGrid : List (List PercType) -> PercGrid
+convertPercussionToGrid stepPercussion =
+    stepPercussion
+        |> List.indexedMap
+            (\stepIdx percTypes ->
+                List.map
+                    (\percType ->
+                        ( Instruments.percRowIdx percType, stepIdx )
+                    )
+                    percTypes
+            )
+        |> List.concat
+        |> Set.fromList
