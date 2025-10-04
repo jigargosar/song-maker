797e21349d39a0f8ece0babd400dc408c44c6069
Jigar Gosar <jigar.gosar@gmail.com>
2025-10-04 20:12:40 +0530

    in progress: GridModel major refactoring. Might have to revert.

--- review/src/ReviewConfig.elm
+++ review/src/ReviewConfig.elm
@@ -3,11 +3,11 @@ module ReviewConfig exposing (config)
 {-| Do not rename the ReviewConfig module or the config function, because
 `elm-review` will look for these.
 
-To add packages that contain rules, add them to this review project using
+    To add packages that contain rules, add them to this review project using
 
-    `elm install author/packagename`
+        elm install author/packagename
 
-when inside the directory containing this file.
+    when inside the directory containing this file.
 
 -}
 
@@ -18,16 +18,24 @@ import NoUnused.Exports
 import NoUnused.Parameters
 import NoUnused.Patterns
 import NoUnused.Variables
-import Review.Rule exposing (Rule)
+import Review.Rule as Rule exposing (Rule)
 
 
 config : List Rule
 config =
-    [ NoUnused.CustomTypeConstructors.rule []
-    , NoUnused.CustomTypeConstructorArgs.rule
-    , NoUnused.Dependencies.rule
-    , NoUnused.Exports.rule
-    , NoUnused.Parameters.rule
-    , NoUnused.Patterns.rule
-    , NoUnused.Variables.rule
-    ]
+    let
+        ignorePatterns =
+            [ "src/Utils.elm" ]
+
+        rawRules =
+            [ NoUnused.CustomTypeConstructors.rule []
+            , NoUnused.CustomTypeConstructorArgs.rule
+            , NoUnused.Dependencies.rule
+            , NoUnused.Exports.rule
+            , NoUnused.Parameters.rule
+            , NoUnused.Patterns.rule
+            , NoUnused.Variables.rule
+            ]
+    in
+    rawRules
+        |> List.map (Rule.ignoreErrorsForFiles ignorePatterns)
