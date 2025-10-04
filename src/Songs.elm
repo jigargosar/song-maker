071341d6c8e5b2f9150ce0f8539d3e51350675f4
Jigar Gosar <jigar.gosar@gmail.com>
2025-10-04 13:51:20 +0530

    fix: update Ode to Joy V2 song configuration to C Major in Songs.elm

--- src/Songs.elm
+++ src/Songs.elm
@@ -169,7 +169,7 @@ odeToJoySong =
             ++ [ [ kick ], [], [ snare ], [], [ kick ], [], [ snare ], [] ]
             ++ [ [ kick ], [], [ snare ], [], [ kick ], [], [ snare ], [] ]
     , scaleType = Scales.Major
-    , rootNote = Scales.D
+    , rootNote = Scales.C
     , bpm = 100
     , octaveStart = 3
     , octaveCount = 2
@@ -235,26 +235,52 @@ jingleBellsSong =
 
 
 {-
-URL: http://localhost:1234/?bpm=100&octaveStart=3&octaveCount=2&pitchGrid=4%2C25%2C5%2C26%2C6%2C14%2C6%2C24%2C6%2C27%2C7%2C0%2C7%2C1%2C7%2C4%2C7%2C8%2C7%2C9%2C7%2C12%2C7%2C16%2C7%2C17%2C7%2C23%2C7%2C28%2C7%2C30%2C8%2C2%2C8%2C6%2C8%2C10%2C8%2C18%2C8%2C22%2C9%2C3%2C9%2C5%2C9%2C11%2C9%2C19%2C9%2C21%2C10%2C20&percGrid=&scale=Major&root=C&bars=4&beatsPerBar=4&subdivisions=2&instrument=Piano&drumKit=Electronic
+   Ode to Joy V1 (D Major):
 
-Settings:
-- Scale: C Major
-- Root: C
-- Octave Start: 3 (2 octaves)
-- Bars: 4, Beats: 4, Subdivisions: 2 (32 total steps)
+   Settings:
+   - Scale: D Major
+   - Root: D
+   - Octave Start: 3 (2 octaves)
+   - Bars: 4, Beats: 4, Subdivisions: 2 (32 total steps)
 
-Pitch Index to Note mapping (C Major scale):
-- 0-6: C3, D3, E3, F3, G3, A3, B3
-- 7-13: C4, D4, E4, F4, G4, A4, B4
+   Pitch Index to Note mapping (D Major scale):
+   - 0-6: D3, E3, F#3, G3, A3, B3, C#4
+   - 7-13: D4, E4, F#4, G4, A4, B4, C#5
 
-Notes by step:
-Bar 1 (steps 0-7):   C4 C4 D4 E4 | C4 E4 D4 --
-Bar 2 (steps 8-15):  C4 C4 D4 E4 | C4 -- B3 --
-Bar 3 (steps 16-23): C4 C4 D4 E4 | F4 E4 D4 C4
-Bar 4 (steps 24-31): B3 G3 A3 B3 | C4 -- C4 --
+   Notes by step:
+   Bar 1 (steps 0-7):   D4 D4 E4 F#4 | D4 F#4 E4 --
+   Bar 2 (steps 8-15):  D4 D4 E4 F#4 | D4 -- C#4 --
+   Bar 3 (steps 16-23): D4 D4 E4 F#4 | G4 F#4 E4 D4
+   Bar 4 (steps 24-31): C#4 A3 B3 C#4 | D4 -- D4 --
 
-Melodic sequence:
-C4-C4-D4-E4-C4-E4-D4 | C4-C4-D4-E4-C4-B3 | C4-C4-D4-E4-F4-E4-D4-C4 | B3-G3-A3-B3-C4-C4
+   Melodic sequence:
+   D4-D4-E4-F#4-D4-F#4-E4 | D4-D4-E4-F#4-D4-C#4 | D4-D4-E4-F#4-G4-F#4-E4-D4 | C#4-A3-B3-C#4-D4-D4
 
-This appears to be a variation or exercise using the C Major scale, moving primarily around C4 (middle C) with some descending motion to G3 at the end.
+   This is Beethoven's "Ode to Joy" melody in D Major, featuring the iconic ascending and descending motif around D4 with descending motion to A3.
+-}
+{-
+   Ode to Joy V2 (C Major):
+
+   URL: http://localhost:1234/?bpm=100&octaveStart=3&octaveCount=2&pitchGrid=4%2C25%2C5%2C26%2C6%2C14%2C6%2C24%2C6%2C27%2C7%2C0%2C7%2C1%2C7%2C4%2C7%2C8%2C7%2C9%2C7%2C12%2C7%2C16%2C7%2C17%2C7%2C23%2C7%2C28%2C7%2C30%2C8%2C2%2C8%2C6%2C8%2C10%2C8%2C18%2C8%2C22%2C9%2C3%2C9%2C5%2C9%2C11%2C9%2C19%2C9%2C21%2C10%2C20&percGrid=&scale=Major&root=C&bars=4&beatsPerBar=4&subdivisions=2&instrument=Piano&drumKit=Electronic
+
+   Settings:
+   - Scale: C Major
+   - Root: C
+   - Octave Start: 3 (2 octaves)
+   - Bars: 4, Beats: 4, Subdivisions: 2 (32 total steps)
+
+   Pitch Index to Note mapping (C Major scale):
+   - 0-6: C3, D3, E3, F3, G3, A3, B3
+   - 7-13: C4, D4, E4, F4, G4, A4, B4
+
+   Notes by step:
+   Bar 1 (steps 0-7):   C4 C4 D4 E4 | C4 E4 D4 --
+   Bar 2 (steps 8-15):  C4 C4 D4 E4 | C4 -- B3 --
+   Bar 3 (steps 16-23): C4 C4 D4 E4 | F4 E4 D4 C4
+   Bar 4 (steps 24-31): B3 G3 A3 B3 | C4 -- C4 --
+
+   Melodic sequence:
+   C4-C4-D4-E4-C4-E4-D4 | C4-C4-D4-E4-C4-B3 | C4-C4-D4-E4-F4-E4-D4-C4 | B3-G3-A3-B3-C4-C4
+
+   This appears to be a variation or exercise using the C Major scale, moving primarily around C4 (middle C) with some descending motion to G3 at the end.
 -}
