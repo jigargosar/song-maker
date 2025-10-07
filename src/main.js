4ff988436d2812bac08ca8aeee27a2b1f4516057
Jigar Gosar <jigar.gosar@gmail.com>
2025-10-07 12:17:27 +0530

    refactor: dynamically load WebAudioFont instruments in main.js

--- src/main.js
+++ src/main.js
@@ -7,20 +7,64 @@ const app = Elm.Main.init({ node: document.getElementById('root') })
 const audioContext = new AudioContext()
 const player = new WebAudioFontPlayer()
 
-/* tones */
-player.loader.decodeAfterLoading(audioContext, '_tone_0000_SBLive_sf2') // piano
-player.loader.decodeAfterLoading(audioContext, '_tone_0120_SBLive_sf2') // marimba 1
-player.loader.decodeAfterLoading(audioContext, '_tone_0400_SBLive_sf2') // strings
-player.loader.decodeAfterLoading(audioContext, '_tone_0450_SBLive_sf2') // strings
-player.loader.decodeAfterLoading(audioContext, '_tone_0730_SBLive_sf2') // flute
-player.loader.decodeAfterLoading(
-  audioContext,
-  '_tone_0730_Aspirin_sf2_file',
-) // flute
+// Instrument definitions: explicit mapping of URL to global variable name
+const instruments = [
+  // Tonal instruments
+  {
+    url: '0000_SBLive_sf2.js',
+    globalVar: '_tone_0000_SBLive_sf2',
+    label: 'piano',
+  },
+  {
+    url: '0120_SBLive_sf2.js',
+    globalVar: '_tone_0120_SBLive_sf2',
+    label: 'marimba',
+  },
+  {
+    url: '0400_SBLive_sf2.js',
+    globalVar: '_tone_0400_SBLive_sf2',
+    label: 'strings',
+  },
+  {
+    url: '0450_SBLive_sf2.js',
+    globalVar: '_tone_0450_SBLive_sf2',
+    label: 'strings 2',
+  },
+  {
+    url: '0730_SBLive_sf2.js',
+    globalVar: '_tone_0730_SBLive_sf2',
+    label: 'flute',
+  },
+  {
+    url: '0730_Aspirin_sf2_file.js',
+    globalVar: '_tone_0730_Aspirin_sf2_file',
+    label: 'flute 2',
+  },
+  // Percussion instruments
+  {
+    url: '12836_0_SBLive_sf2.js',
+    globalVar: '_drum_36_0_SBLive_sf2',
+    label: 'kick',
+  },
+  {
+    url: '12838_0_SBLive_sf2.js',
+    globalVar: '_drum_38_0_SBLive_sf2',
+    label: 'snare',
+  },
+]
 
-/* percussion */
-player.loader.decodeAfterLoading(audioContext, '_drum_36_0_SBLive_sf2') // kick
-player.loader.decodeAfterLoading(audioContext, '_drum_38_0_SBLive_sf2') // snare
+const urlPrefix = 'https://surikov.github.io/webaudiofontdata/sound/'
+
+// Load all instruments dynamically
+instruments.forEach((instrument) => {
+  const url = urlPrefix + instrument.url
+  console.log(`Loading instrument: ${instrument.label} from ${url}`)
+  player.loader.startLoad(
+    audioContext,
+    url,
+    instrument.globalVar,
+  )
+})
 
 // Handle unified note playback
 app.ports.playNote.subscribe((data) => {
